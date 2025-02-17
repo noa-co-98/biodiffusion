628e19ecd3f9bf1dc36c5dfb9075e8daa5a3eec3
noa-co-98 <83413228+noa-co-98@users.noreply.github.com>
2025-02-17 20:03:32 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -18,8 +18,6 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.preprocessing import StandardScaler
 
-cifar_labels = "airplane,automobile,bird,cat,deer,dog,frog,horse,ship,truck".split(",")
-alphabet_labels = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split(" ")
 BASE_DIR =  Path('/content/data_publish_v2') / 'data_publish_v2'
 label_mapping = {
     'bag1': 0, 'bag2': 0, 'bag_low2': 0, 'bag_low3': 0, 'bag_normal1': 0,
@@ -51,67 +49,6 @@ def set_seed(s, reproducible=False):
 def untar_data(url, force_download=False, base='./datasets'):
     d = FastDownload(base=base)
     return d.get(url, force=force_download, extract_key='data')
-
-
-def get_alphabet(args):
-    get_kaggle_dataset("alphabet", "thomasqazwsxedc/alphabet-characters-fonts-dataset")
-    train_transforms = T.Compose([
-        T.Grayscale(),
-        T.ToTensor(),])
-    train_dataset = torchvision.datasets.ImageFolder(root="./alphabet/Images/Images/", transform=train_transforms)
-    if args.slice_size>1:
-        train_dataset = torch.utils.data.Subset(train_dataset, indices=range(0, len(train_dataset), args.slice_size))
-    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
-    return train_dataloader, None
-
-def get_cifar(cifar100=False, img_size=64):
-    "Download and extract CIFAR"
-    cifar10_url = 'https://s3.amazonaws.com/fast-ai-sample/cifar10.tgz'
-    cifar100_url = 'https://s3.amazonaws.com/fast-ai-imageclas/cifar100.tgz'
-    if img_size==32:
-        return untar_data(cifar100_url if cifar100 else cifar10_url)
-    else:
-        get_kaggle_dataset("datasets/cifar10_64", "joaopauloschuler/cifar10-64x64-resized-via-cai-super-resolution")
-        return Path("datasets/cifar10_64/cifar10-64")
-
-def get_kaggle_dataset(dataset_path, # Local path to download dataset to
-                dataset_slug, # Dataset slug (ie "zillow/zecon")
-                unzip=True, # Should it unzip after downloading?
-                force=False # Should it overwrite or error if dataset_path exists?
-               ):
-    '''Downloads an existing dataset and metadata from kaggle'''
-    if not force and Path(dataset_path).exists():
-        return Path(dataset_path)
-    api.dataset_metadata(dataset_slug, str(dataset_path))
-    api.dataset_download_files(dataset_slug, str(dataset_path))
-    if unzip:
-        zipped_file = Path(dataset_path)/f"{dataset_slug.split('/')[-1]}.zip"
-        import zipfile
-        with zipfile.ZipFile(zipped_file, 'r') as zip_ref:
-            zip_ref.extractall(Path(dataset_path))
-        zipped_file.unlink()
-
-def one_batch(dl):
-    batch = next(iter(dl))
-    signals, labels = batch['signal'], batch['label']
-    print("type check")
-    print(type(signals))  # Should be <class 'torch.Tensor'>
-    print(type(labels))   # Should be <class 'torch.Tensor'>
-    return signals, labels
-
-def plot_images(images):
-    plt.figure(figsize=(32, 32))
-    plt.imshow(torch.cat([
-        torch.cat([i for i in images.cpu()], dim=-1),
-    ], dim=-2).permute(1, 2, 0).cpu())
-    plt.show()
-
-
-def save_images(images, path, **kwargs):
-    grid = torchvision.utils.make_grid(images, **kwargs)
-    ndarr = grid.permute(1, 2, 0).to('cpu').numpy()
-    im = Image.fromarray(ndarr)
-    im.save(path)
 
 
 class MultiChannelSignalDataset(Dataset):
